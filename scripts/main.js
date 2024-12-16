b13f976fb7175aa162dfb8bfcdc13a8cb25da21c
alex.linux <183350467+alx-unix@users.noreply.github.com>
2024-12-16 10:52:34 +0100

    added random colors, and clearGrid button

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 function createGrid(grids){
+    
+    
     const mainContainer = document.querySelector(".container");
-    mainContainer.style.height = "960px";
+    mainContainer.style.height = "720px";
     mainContainer.style.display = "flex";
-    mainContainer.style.width = "960px"
+    mainContainer.style.width = "720px"
     mainContainer.style.flexDirection = "column";
     mainContainer.style.backgroundColor = "white";
     
@@ -22,27 +24,41 @@ function createGrid(grids){
             row.appendChild(gridItem);
 
             gridItem.addEventListener("mouseenter",()=>{
-                gridItem.style.backgroundColor ="yellow"
+                gridItem.style.backgroundColor =`rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`;
             })
         }
         mainContainer.appendChild(row);
         
     }
 }
-createGrid(20)
 
 function deleteGrid(){
     const mainContainer = document.querySelector(".container");
     mainContainer.innerHTML=""
 }
 
-const myButton = document.getElementById('btn')
+function clearGrid(){
+    const grids = document.querySelectorAll(".gridItem")
+    grids.style.backgroundColor = "white"
+}
+
+
+createGrid(20)
+
+const myButton = document.getElementById('btn');
 myButton.addEventListener('click',()=>{
     const userPromt = prompt("How many grids would you like to put");
     deleteGrid()
     createGrid(userPromt)
 })
 
+const clearButton = document.getElementById('clear');
+clearButton.addEventListener('click',()=>{
+    const grids = document.querySelectorAll(".gridItem");
+    for (let i=0; i < grids.length; i++){
+        grids[i].style.backgroundColor = "white";
+    }
+})
 
 
 
